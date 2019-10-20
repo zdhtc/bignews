2c573821e206169908d64bc6653346e6f6792e47
zdhtc <1515501145@qq.com>
2019-10-20 18:10:52 +0800

    用户信息修改功能

--- admin/libs/js/user.js
+++ admin/libs/js/user.js
@@ -1,20 +1,62 @@
-$(function(){
+$(function () {
 
   //请求个人用户信息详情
   $.ajax({
     url: Bignew.user_detail,
     dataType: 'json',
-    success(res){
-      
+    success(res) {
+
       for (const key in res.data) {
-       $('.' + key).val(res.data[key]);
+        $('.' + key).val(res.data[key]);
       }
 
-      $('.user_pic').attr('src',res.data.userPic)
+      $('.user_pic').attr('src', res.data.userPic)
     }
   })
 
 
-  
+  //图片预览功能
+  $('#exampleInputFile').change(function () {
+    var url = URL.createObjectURL(this.files[0]);
+    $('.user_pic').attr('src', url)
+  })
+
+  //编辑之后提交
+  $('.btn-edit').click(function (e) {
+    e.preventDefault();
+    var flag = true;
+    $('#form input:not([type="file"])').each((i, item) => {
+      //信息不全提醒用户
+      if ($(item).val().trim().length === 0) {
+        $(".modal-body").text("用户信息不能为空,请重新输入!");
+        $("#myModal").modal('show');
+        flag = false;
+        return;
+      }
+    })
+
+    //用户信息完整提交信息
+    if (flag) {
+      var formObj = $('#form')[0];
+      $.ajax({
+        type: 'post',
+        url: Bignew.user_edit,
+        data: new FormData(formObj),
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        success(res) {
+          if (res.code === 200) {
+            window.parent.location.reload();
+          } else {
+            $(".modal-body").text("信息更新失败，请稍后重试!");
+            $("#myModal").modal('show');
+          }
+        }
+      })
+    }
+
+
+  })
 
 })
